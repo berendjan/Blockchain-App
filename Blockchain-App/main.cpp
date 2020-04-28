63fe02e16c88bba34df8c293376eb3153ce53990
Berend-Jan Lange <lange.berendjan@gmail.com>
2020-04-28 17:55:09 +0200

    Initial Commit

---
+++ Blockchain-App/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Blockchain-App
+//
+//  Created by Berend-Jan Lange on 28/04/2020.
+//  Copyright © 2020 Berend-Jan Lange. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
